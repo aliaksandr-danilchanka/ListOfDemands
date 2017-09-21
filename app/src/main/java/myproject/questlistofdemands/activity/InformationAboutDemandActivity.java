33753f0a0b0ef0218d9c11c10d318316da1f8e1a
Aliaksandr <aliaksandr.danilchanka@gmail.com>
2017-09-21 16:33:41 +0200

    Add new activity.

---
+++ app/src/main/java/myproject/questlistofdemands/activity/InformationAboutDemandActivity.java
@@ -0,0 +1,15 @@
+package myproject.questlistofdemands.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import myproject.questlistofdemands.R;
+
+public class InformationAboutDemandActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_information_about_demand);
+    }
+}
