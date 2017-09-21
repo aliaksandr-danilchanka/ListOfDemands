f64e99594c89e54351f091791931c5bf0c7ab109
Aliaksandr <aliaksandr.danilchanka@gmail.com>
2017-09-21 15:34:10 +0200

    Modify ListOfDemandsFragment.java file.(add code for work with api and add code for result work)

--- app/src/main/java/myproject/questlistofdemands/fragment/ListOfDemandsFragment.java
+++ app/src/main/java/myproject/questlistofdemands/fragment/ListOfDemandsFragment.java
@@ -7,6 +7,9 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import java.util.ArrayList;
 
@@ -16,19 +19,22 @@ import myproject.questlistofdemands.R;
 import myproject.questlistofdemands.adapter.DemandAdapter;
 import myproject.questlistofdemands.api.RestHelper;
 import myproject.questlistofdemands.model.Demand;
-import myproject.questlistofdemands.model.DemandListResponse;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-/**
- * Created by Aliaksandr on 9/19/2017.
- */
-
 public class ListOfDemandsFragment extends Fragment {
+
+    public static final String DEMAND_KEY = "DEMAND_KEY";
 
     @BindView(R.id.recycler_view)
     RecyclerView mRecyclerView;
+    @BindView(R.id.search_bar)
+    FrameLayout mSearchBar;
+    @BindView(R.id.view_error)
+    RelativeLayout mViewError;
+    @BindView(R.id.view_no_results)
+    LinearLayout mViewNoResults;
 
     private ArrayList<Demand> mDemands;
 
@@ -40,46 +46,90 @@ public class ListOfDemandsFragment extends Fragment {
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
         mRecyclerView.setLayoutManager(linearLayoutManager);
         mRecyclerView.setHasFixedSize(true);
+        showRecyclerView();
 
-        loadData();
+        if (savedInstanceState != null) {
+            mDemands = savedInstanceState.getParcelableArrayList(DEMAND_KEY);
+            if (mDemands != null) {
+                initializeAdapter();
+            } else {
+                showProgressBarView();
+                loadData();
+            }
+        } else {
+            showProgressBarView();
+            loadData();
+        }
+
+        if (mDemands != null) {
+            initializeAdapter();
+        }
 
         return v;
     }
 
-//    private void loadData() {
-//        RestHelper.getInterface().getDemandInformation(1).enqueue(new Callback<Demand>() {
-//            @Override
-//            public void onResponse(Call<Demand> call, Response<Demand> response) {
-//                if (mDemands == null) mDemands = new ArrayList<>();
-//                mDemands.clear();
-//                mDemands.add(response.body());
-//                initializeAdapter();
-//            }
-//
-//            @Override
-//            public void onFailure(Call<Demand> call, Throwable t) {
-//            }
-//        });
-//    }
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putParcelableArrayList(DEMAND_KEY, mDemands);
+    }
 
     private void loadData() {
-        RestHelper.getInterface().getDemandList(4, 4).enqueue(new Callback<DemandListResponse>() {
+        RestHelper.getInterface().getDemandList(5, 5).enqueue(new Callback<ArrayList<Demand>>() {
             @Override
-            public void onResponse(Call<DemandListResponse> call, Response<DemandListResponse> response) {
+            public void onResponse(Call<ArrayList<Demand>> call, Response<ArrayList<Demand>> response) {
                 if (mDemands == null) mDemands = new ArrayList<>();
                 mDemands.clear();
-                mDemands.addAll(response.body().getDemands());
-                initializeAdapter();
+                mDemands = response.body();
+                if(mDemands != null) {
+                    if (mDemands.isEmpty()) {
+                        showNoResultsView();
+                    } else {
+                        initializeAdapter();
+                        showRecyclerView();
+                    }
+                }else{
+                    showNoResultsView();
+                }
             }
 
             @Override
-            public void onFailure(Call<DemandListResponse> call, Throwable t) {
+            public void onFailure(Call<ArrayList<Demand>> call, Throwable t) {
+                showErrorView();
             }
         });
     }
 
     private void initializeAdapter() {
-        DemandAdapter rvAdapter = new DemandAdapter(mDemands);
+        DemandAdapter rvAdapter = new DemandAdapter(mDemands, getContext());
         mRecyclerView.setAdapter(rvAdapter);
+    }
+
+    private void showRecyclerView() {
+        mRecyclerView.setVisibility(View.VISIBLE);
+        mSearchBar.setVisibility(View.GONE);
+        mViewError.setVisibility(View.GONE);
+        mViewNoResults.setVisibility(View.GONE);
+    }
+
+    private void showProgressBarView() {
+        mRecyclerView.setVisibility(View.GONE);
+        mSearchBar.setVisibility(View.VISIBLE);
+        mViewError.setVisibility(View.GONE);
+        mViewNoResults.setVisibility(View.GONE);
+    }
+
+    private void showErrorView() {
+        mRecyclerView.setVisibility(View.GONE);
+        mSearchBar.setVisibility(View.GONE);
+        mViewError.setVisibility(View.VISIBLE);
+        mViewNoResults.setVisibility(View.GONE);
+    }
+
+    private void showNoResultsView() {
+        mRecyclerView.setVisibility(View.GONE);
+        mSearchBar.setVisibility(View.GONE);
+        mViewError.setVisibility(View.GONE);
+        mViewNoResults.setVisibility(View.VISIBLE);
     }
 }
