eb30aee9183bd0755a6a3f2d9da36fa4588ea0e5
Aliaksandr <aliaksandr.danilchanka@gmail.com>
2017-09-21 18:05:30 +0200

    Add clickListener for finish activity.

--- app/src/main/java/myproject/questlistofdemands/activity/base/BaseActivity.java
+++ app/src/main/java/myproject/questlistofdemands/activity/base/BaseActivity.java
@@ -1,10 +1,23 @@
 package myproject.questlistofdemands.activity.base;
 
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 /**
  * Created by Aliaksandr on 9/19/2017.
  */
 
 public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
