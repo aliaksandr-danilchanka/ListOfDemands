873094feb5fbe1db7077570e9cc5c5a0781be07a
Aliaksandr <aliaksandr.danilchanka@gmail.com>
2017-09-21 15:35:16 +0200

    Add parcelable in Demand.java file and remove  DemandListResponse.java file.

--- app/src/main/java/myproject/questlistofdemands/model/Demand.java
+++ app/src/main/java/myproject/questlistofdemands/model/Demand.java
@@ -1,169 +1,205 @@
 package myproject.questlistofdemands.model;
 
-/**
- * Created by Aliaksandr on 9/19/2017.
- */
+import android.os.Parcel;
+import android.os.Parcelable;
 
-public class Demand {
+public class Demand implements Parcelable {
 
+    public static final Creator<Demand> CREATOR = new Creator<Demand>() {
+        @Override
+        public Demand createFromParcel(Parcel in) {
+            return new Demand(in);
+        }
+
+        @Override
+        public Demand[] newArray(int size) {
+            return new Demand[size];
+        }
+    };
     private int id;
     private String creationUtcTime;
-    private creatorBean creator;
-    private customerCompanyBean customerCompany;
-    private stateBean state;
-    private addressBean address;
+    private CreatorBean creator;
+    private CustomerCompanyBean customerCompany;
+    private StateBean state;
+    private AddressBean address;
     private int categoryId;
     private String validTillUtcTime;
-    private intervalBean interval;
+    private IntervalBean interval;
     private String title;
     private int currencyId;
     private Object bestOffer;
     private int offerCount;
 
+    protected Demand(Parcel in) {
+        id = in.readInt();
+        creationUtcTime = in.readString();
+        categoryId = in.readInt();
+        validTillUtcTime = in.readString();
+        title = in.readString();
+        currencyId = in.readInt();
+        offerCount = in.readInt();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeInt(id);
+        parcel.writeString(creationUtcTime);
+        parcel.writeInt(categoryId);
+        parcel.writeString(validTillUtcTime);
+        parcel.writeString(title);
+        parcel.writeInt(currencyId);
+        parcel.writeInt(offerCount);
+    }
+
     public int getId() {
         return id;
-    }
-
-    public String getCreationUtcTime() {
-        return creationUtcTime;
-    }
-
-    public creatorBean getCreator() {
-        return creator;
-    }
-
-    public customerCompanyBean getCustomerCompany() {
-        return customerCompany;
-    }
-
-    public stateBean getState() {
-        return state;
-    }
-
-    public addressBean getAddress() {
-        return address;
-    }
-
-    public int getCategoryId() {
-        return categoryId;
-    }
-
-    public String getValidTillUtcTime() {
-        return validTillUtcTime;
-    }
-
-    public intervalBean getInterval() {
-        return interval;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public int getCurrencyId() {
-        return currencyId;
-    }
-
-    public Object getBestOffer() {
-        return bestOffer;
-    }
-
-    public int getOfferCount() {
-        return offerCount;
     }
 
     public void setId(int id) {
         this.id = id;
     }
 
+    public String getCreationUtcTime() {
+        return creationUtcTime;
+    }
+
     public void setCreationUtcTime(String creationUtcTime) {
         this.creationUtcTime = creationUtcTime;
     }
 
-    public void setCreator(creatorBean creator) {
+    public CreatorBean getCreator() {
+        return creator;
+    }
+
+    public void setCreator(CreatorBean creator) {
         this.creator = creator;
     }
 
-    public void setCustomerCompany(customerCompanyBean customerCompany) {
+    public CustomerCompanyBean getCustomerCompany() {
+        return customerCompany;
+    }
+
+    public void setCustomerCompany(CustomerCompanyBean customerCompany) {
         this.customerCompany = customerCompany;
     }
 
-    public void setState(stateBean state) {
+    public StateBean getState() {
+        return state;
+    }
+
+    public void setState(StateBean state) {
         this.state = state;
     }
 
-    public void setAddress(addressBean address) {
+    public AddressBean getAddress() {
+        return address;
+    }
+
+    public void setAddress(AddressBean address) {
         this.address = address;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
     }
 
     public void setCategoryId(int categoryId) {
         this.categoryId = categoryId;
     }
 
+    public String getValidTillUtcTime() {
+        return validTillUtcTime;
+    }
+
     public void setValidTillUtcTime(String validTillUtcTime) {
         this.validTillUtcTime = validTillUtcTime;
     }
 
-    public void setInterval(intervalBean interval) {
+    public IntervalBean getInterval() {
+        return interval;
+    }
+
+    public void setInterval(IntervalBean interval) {
         this.interval = interval;
+    }
+
+    public String getTitle() {
+        return title;
     }
 
     public void setTitle(String title) {
         this.title = title;
     }
 
+    public int getCurrencyId() {
+        return currencyId;
+    }
+
     public void setCurrencyId(int currencyId) {
         this.currencyId = currencyId;
+    }
+
+    public Object getBestOffer() {
+        return bestOffer;
     }
 
     public void setBestOffer(Object bestOffer) {
         this.bestOffer = bestOffer;
     }
 
+    public int getOfferCount() {
+        return offerCount;
+    }
+
     public void setOfferCount(int offerCount) {
         this.offerCount = offerCount;
     }
 
-    public static class creatorBean {
+    public static class CreatorBean {
 
         private int id;
         private String firstName;
         private String lastName;
-        private pictureBean picture;
+        private PictureBean picture;
 
         public int getId() {
             return id;
-        }
-
-        public String getFirstName() {
-            return firstName;
-        }
-
-        public String getLastName() {
-            return lastName;
-        }
-
-        public pictureBean getPicture() {
-            return picture;
         }
 
         public void setId(int id) {
             this.id = id;
         }
 
+        public String getFirstName() {
+            return firstName;
+        }
+
         public void setFirstName(String firstName) {
             this.firstName = firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
         }
 
         public void setLastName(String lastName) {
             this.lastName = lastName;
         }
 
-        public void setPicture(pictureBean picture) {
+        public PictureBean getPicture() {
+            return picture;
+        }
+
+        public void setPicture(PictureBean picture) {
             this.picture = picture;
         }
 
-        public static class pictureBean {
+        public static class PictureBean {
 
             private int id;
             private String token;
@@ -173,20 +209,20 @@ public class Demand {
                 return id;
             }
 
-            public String getToken() {
-                return token;
-            }
-
-            public boolean isIsUploaded() {
-                return isUploaded;
-            }
-
             public void setId(int id) {
                 this.id = id;
             }
 
+            public String getToken() {
+                return token;
+            }
+
             public void setToken(String token) {
                 this.token = token;
+            }
+
+            public boolean isIsUploaded() {
+                return isUploaded;
             }
 
             public void setIsUploaded(boolean isUploaded) {
@@ -195,10 +231,10 @@ public class Demand {
         }
     }
 
-    public static class customerCompanyBean {
+    public static class CustomerCompanyBean {
 
         private int id;
-        private Object name;
+        private String name;
         private boolean isVerified;
         private boolean isHidden;
         private int rating;
@@ -208,44 +244,44 @@ public class Demand {
             return id;
         }
 
-        public Object getName() {
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
             return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
         }
 
         public boolean isIsVerified() {
             return isVerified;
         }
 
-        public boolean isIsHidden() {
-            return isHidden;
-        }
-
-        public int getRating() {
-            return rating;
-        }
-
-        public Object getLogo() {
-            return logo;
-        }
-
-        public void setId(int id) {
-            this.id = id;
-        }
-
-        public void setName(Object name) {
-            this.name = name;
-        }
-
         public void setIsVerified(boolean isVerified) {
             this.isVerified = isVerified;
+        }
+
+        public boolean isIsHidden() {
+            return isHidden;
         }
 
         public void setIsHidden(boolean isHidden) {
             this.isHidden = isHidden;
         }
 
+        public int getRating() {
+            return rating;
+        }
+
         public void setRating(int rating) {
             this.rating = rating;
+        }
+
+        public Object getLogo() {
+            return logo;
         }
 
         public void setLogo(Object logo) {
@@ -253,7 +289,7 @@ public class Demand {
         }
     }
 
-    public static class stateBean {
+    public static class StateBean {
 
         private String utcTime;
         private int value;
@@ -262,12 +298,12 @@ public class Demand {
             return utcTime;
         }
 
-        public int getValue() {
-            return value;
-        }
-
         public void setUtcTime(String utcTime) {
             this.utcTime = utcTime;
+        }
+
+        public int getValue() {
+            return value;
         }
 
         public void setValue(int value) {
@@ -275,7 +311,7 @@ public class Demand {
         }
     }
 
-    public static class addressBean {
+    public static class AddressBean {
 
         private int id;
         private String placeId;
@@ -283,74 +319,83 @@ public class Demand {
         private String street;
         private String buildingNumber;
         private String city;
+        private String zipCode;
         private String countryCode;
-        private gpsBean gps;
+        private GpsBean gps;
 
         public int getId() {
             return id;
-        }
-
-        public String getPlaceId() {
-            return placeId;
-        }
-
-        public String getLabel() {
-            return label;
-        }
-
-        public String getStreet() {
-            return street;
-        }
-
-        public String getBuildingNumber() {
-            return buildingNumber;
-        }
-
-        public String getCity() {
-            return city;
-        }
-
-        public String getCountryCode() {
-            return countryCode;
-        }
-
-        public gpsBean getGps() {
-            return gps;
         }
 
         public void setId(int id) {
             this.id = id;
         }
 
+        public String getPlaceId() {
+            return placeId;
+        }
+
         public void setPlaceId(String placeId) {
             this.placeId = placeId;
+        }
+
+        public String getLabel() {
+            return label;
         }
 
         public void setLabel(String label) {
             this.label = label;
         }
 
+        public String getStreet() {
+            return street;
+        }
+
         public void setStreet(String street) {
             this.street = street;
+        }
+
+        public String getBuildingNumber() {
+            return buildingNumber;
         }
 
         public void setBuildingNumber(String buildingNumber) {
             this.buildingNumber = buildingNumber;
         }
 
+        public String getCity() {
+            return city;
+        }
+
         public void setCity(String city) {
             this.city = city;
+        }
+
+        public String getZipCode() {
+            return zipCode;
+        }
+
+        public void setZipCode(String zipCode) {
+            this.zipCode = zipCode;
+        }
+
+        public String getCountryCode() {
+            return countryCode;
         }
 
         public void setCountryCode(String countryCode) {
             this.countryCode = countryCode;
         }
 
-        public void setGps(gpsBean gps) {
+        public GpsBean getGps() {
+            return gps;
+        }
+
+        public void setGps(GpsBean gps) {
             this.gps = gps;
         }
 
-        public static class gpsBean {
+        public static class GpsBean {
 
             private double latitude;
             private double longitude;
@@ -359,12 +404,12 @@ public class Demand {
                 return latitude;
             }
 
-            public double getLongitude() {
-                return longitude;
-            }
-
             public void setLatitude(double latitude) {
                 this.latitude = latitude;
+            }
+
+            public double getLongitude() {
+                return longitude;
             }
 
             public void setLongitude(double longitude) {
@@ -373,7 +418,7 @@ public class Demand {
         }
     }
 
-    public static class intervalBean {
+    public static class IntervalBean {
 
         private String fromUtcTime;
         private String toUtcTime;
@@ -382,12 +427,12 @@ public class Demand {
             return fromUtcTime;
         }
 
-        public String getToUtcTime() {
-            return toUtcTime;
-        }
-
         public void setFromUtcTime(String fromUtcTime) {
             this.fromUtcTime = fromUtcTime;
+        }
+
+        public String getToUtcTime() {
+            return toUtcTime;
         }
 
         public void setToUtcTime(String toUtcTime) {
